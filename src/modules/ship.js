e6f2dcf297f356554885437c82338c9851fc0713
dustics4 <konrad.babowicz21@gmail.com>
2024-09-26 17:47:18 +0100

    Created ship class and functions hit and isSunk

---
+++ src/modules/ship.js
@@ -0,0 +1,14 @@
+class Ship {
+    constructor(length){
+        this.length = length;
+        this.hit = 0;
+    }
+
+    hit(){ //function increases the number of hits on a ship
+        
+    }
+
+    isSunk(){ //function caclulates if the ship has been sunk , based on the length and number of times it has been hit
+
+    }
+}
