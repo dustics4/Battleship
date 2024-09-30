33bc347288223c4d7e0c31490053d0d011ebafe3
dustics4 <konrad.babowicz21@gmail.com>
2024-09-30 17:55:24 +0100

    Completed another test

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -3,16 +3,17 @@ const { ModuleFilenameHelpers } = require("webpack");
 class Ship {
     constructor(length){
         this.length = length;
-        this.hit = 0;
+        this.hits = 0;
     }
 
     hit(){ //function increases the number of hits on a ship
-        
+        this.hits++;
     }
 
     isSunk(){ //function caclulates if the ship has been sunk , based on the length and number of times it has been hit
 
     }
+
 }
 
 export default Ship;
--- src/modules/tests/ship.test.js
+++ src/modules/tests/ship.test.js
@@ -4,9 +4,22 @@ import Ship from "../ship.js"
 describe("Ship class" , ()=> {
     test('should initialize with correct length and hits', () => {
         let ship = new Ship(4);
+        console.log(ship);
+
+        expect(typeof ship.length).toBe('number');
+        expect(typeof ship.hits).toBe('number');
 
         expect(ship.length).toBe(4);
 
-        expect(ship.hit).toBe(0);
+        expect(ship.hits).toBe(0);
     })
+
+    test('Should show correct hit amount ', () => {
+        let ship = new Ship(4);
+
+        ship.hit();
+        expect(ship.hits).toBe(1);
+    })
+
+    //next we test the isshipsunk function
 })
