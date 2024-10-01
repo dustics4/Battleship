84f5287bbbf6e7b2c45e92c7ae5ac643c5c1521f
dustics4 <konrad.babowicz21@gmail.com>
2024-10-01 17:27:31 +0100

    Created Class and functions

---
+++ src/modules/gameboard.js
@@ -0,0 +1,23 @@
+class Gameboard {
+    constructor(){
+        this.board = Array(10).fill(null).map(() => Array(10).fill(null));
+        this.missedAttacks = [];
+        this.ships = [];
+    }
+
+    placeShip(ship, coordinates){
+
+    }
+
+    receiveAttack(x,y){
+
+    }
+
+    renderBoard(){
+
+    }
+
+    allShipsSunk(){
+        
+    }
+}
