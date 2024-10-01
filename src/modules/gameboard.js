ecf78d6670e12f787d5a9c63bf9eace17c3411b9
dustics4 <konrad.babowicz21@gmail.com>
2024-10-01 17:45:03 +0100

    Created borders and so ships dont overlap

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -6,6 +6,15 @@ class Gameboard {
     }
 
     placeShip(ship, coordinates){
+        for(let [x,y] of coordinates){
+            if(x < 0 || x >= 10 || y < 0 || y >= 10){
+                throw new Error ('Invalid Placement: Ship is out of bounds');
+            }
+            if(this.board[x][y] !== null){
+                throw new Error ('Invalid Placement : ship overlaps with another ship');
+            }
+        }
+
         this.ships.push(ship);
         coordinates.forEach(([x,y]) => {
             this.board[x][y] = ship;
