00114eb7415955b6f9ba7deb62aea36493f61ea6
Konrad <konrad.babowicz21@gmail.com>
2024-10-06 18:43:42 +0100

    Setting up beforeEach to mock functions

--- src/modules/tests/player.test.js
+++ src/modules/tests/player.test.js
@@ -2,9 +2,17 @@ import Player from "../player.js";
 import Gameboard from "../gameboard.js";
 
 describe("Attack Enemy ", () => {
+    beforeEach(() => {
 
+    })
+
+    test("Takes correct parameters ", () => {
+
+    })
 })
 
 describe("Random Attack ", () => {
-    
+    beforeEach(() => {
+
+    })
 })
