6604e92519fb8eba9ea8970c2af0ee3120cf3b27
Konrad <konrad.babowicz21@gmail.com>
2024-10-06 16:53:18 +0100

    Created Player class

---
+++ src/modules/player.js
@@ -0,0 +1,28 @@
+import Gameboard from "./gameboard.js";
+
+class Player {
+    constructor(isComputer){
+        this.gameboard = new Gameboard();
+        this.isComputer = isComputer;
+    }
+
+    attackEnemy(enemyBoard , coordinates){ //human
+
+    }
+
+    randomAttack(enemyBoard){ //computerAtack
+        //math.random for attack on x,y
+    }
+}
+
+class isComputer extends Player{
+    constructor(){
+        super(true);
+    }
+
+    randomAttack(enemyBoard){
+        
+    }
+}
+
+//subclass create for computer player
