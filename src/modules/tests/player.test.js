5515457155d7b5b6e99be4b941f4a2e4bda007a3
Konrad <konrad.babowicz21@gmail.com>
2024-10-06 19:54:03 +0100

    Cleaning up mocks

--- src/modules/tests/player.test.js
+++ src/modules/tests/player.test.js
@@ -1,13 +1,33 @@
 import Player from "../player.js";
 import Gameboard from "../gameboard.js";
+import { afterEach, jest } from "@jest/globals";
 
-describe("Attack Enemy ", () => {
+jest.mock("./gameboard.js");
+
+
+describe("Player class test ", () => {
+    let player , gameboard;
     beforeEach(() => {
+        player = new Player(false);
+        enemyBoard = new Gameboard;
 
+        jest.spyOn(enemyBoard, "receiveAttack");
     })
 
-    test("Takes correct parameters ", () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
 
+    describe("Attack Enemy", () => {
+        beforeEach(() => {
+    
+        })
+    })
+
+    describe("Random Attack ", () => {
+        beforeEach(() => {
+    
+        })
     })
 })
 
