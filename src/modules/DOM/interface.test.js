d13eed53a82536dd0dd39b7bb31d8e4f14512635
Konrad <konrad.babowicz21@gmail.com>
2024-10-09 21:01:58 +0100

    Setting up test:

--- src/modules/DOM/interface.test.js
+++ src/modules/DOM/interface.test.js
@@ -2,5 +2,11 @@ import { describe } from "@jest/globals";
 import Interface from "./interface.js";
 
 describe("Interface Test " , () => {
-    
-})
+    test("Should create 10x10 board ", () => {
+        document.body.innerHTML = `
+        <div id = "player-board">
+        <div class = "gameboard"> </div>
+        </div>
+        `
+    })
+})  
