066776d644eaab50ae6454ed152a3cffaf36b7af
dustics4 <konrad.babowicz21@gmail.com>
2024-11-19 22:08:30 +0000

    Issue resolved, was placing ship wrong

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const computer = new Player(true);
 const ship = new Ship(1);
 player.gameboard.placeShip(ship, [[0,0]]);
 
-player.gameboard.placeShip(1, [[0,0]])
 computer.attackEnemy(player.gameboard, [0,0])
 
 //console.log(player.gameboard);
