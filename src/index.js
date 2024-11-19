346362a9cf6bcc7ab83c98ed6a2e3041f855156d
dustics4 <konrad.babowicz21@gmail.com>
2024-11-19 22:07:44 +0000

    trying to resolve erro when placing ship

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Interface from "./modules/DOM/interface.js";
 import Player from "./modules/player.js";
 import Gameboard from "./modules/gameboard.js";
+import Ship from "./modules/ship.js";
 
 Interface.createBoardElement("player");
 Interface.createBoardElement("computer");
 const player = new Player(false);
 const computer = new Player(true);
+const ship = new Ship(1);
+player.gameboard.placeShip(ship, [[0,0]]);
 
-player.gameboard.placeShip(1, [0,0])
+player.gameboard.placeShip(1, [[0,0]])
 computer.attackEnemy(player.gameboard, [0,0])
 
 //console.log(player.gameboard);
